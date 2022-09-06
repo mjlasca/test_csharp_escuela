fda30e29342ae7987bfd2e7bcec8307a8e1490b7
Mario Lasca <mjlasca@gmail.com>
2022-09-06 00:09:54 -0500

    Paso CURSO

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,30 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+
+        public string Nombre {
+            get{return nombre ; }
+            set{ nombre = value ;}
+        }
+
+
+
+        public int anoCreacion { get; set; }
+        public int pais { get; set; }
+        public int ciudad { get; set; }
+        public TiposEscuela TipoEscuela {get; set;}
+
+
+        public Escuela (string nombre, string pais = "",  string ciudad = "" ) 
+        { 
+            (Nombre) = (nombre);
+          
+        }
+
+        public override string ToString(){
+            return "PAIS  {this.pais} Nombre "+ this.nombre +" TIPO "+ this.TipoEscuela;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace ETAPA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Escuela  escuela = new Escuela("Platzi School" , "Colombia");
+            escuela.TipoEscuela = TiposEscuela.PreEscolar;
+
+            Curso curso1 = new Curso(){
+                Nombre = "101",
+            };
+            Curso curso2 = new Curso(){
+                Nombre = "102",
+            };
+            Curso curso3 = new Curso(){
+                Nombre = "103",
+            };
+            
+
+            Console.WriteLine(escuela.ToString());      
+            Console.WriteLine("---------------------------------------------------------------------------");      
+            Console.WriteLine($" Curso 1 : {curso1.Nombre} ID : {curso1.UniqueId} " );
+            Console.WriteLine($" Curso 2 : {curso2.Nombre} ID : {curso2.UniqueId} " );
+        }
+    }
+}
