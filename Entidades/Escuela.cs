31912461a5bdbe54559a2d10c9c692900bc2ad36
Mario Lasluisa <mjlasca@gmail.com>
2022-09-15 19:27:36 -0500

    borré todo

--- Entidades/Escuela.cs
+++
@@ -1,8 +0,0 @@
-namespace CoreEscuela
-{
-    class Escuela
-    {
-        public string nombre {get;set;}
-        public string anoFundacion {get;set;}
-    }
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# test_csharp_escuela
-Test School
-Curso de csharp proyecto escuela
